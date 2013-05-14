e979886d92637f53a55601988d2ef8b9365dedf3
Nicolás Múnera <nicolasmunera@MacBook-Pro-de-Nicolas.local>
2013-05-14 17:44:34 -0500

    Initial Commit

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Dijkstra
+//
+//  Created by Nicolás Múnera on 14/05/13.
+//  Copyright (c) 2013 Nicolás Múnera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
